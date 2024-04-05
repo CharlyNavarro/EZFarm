8f2160b8d40f34b4744c5286708fb4781afe8116
NintenCarlos <juancarlossantoyo0@gmail.com>
2024-04-05 09:34:20 -0600

    EZ farm promocional 2

--- frontend/src/pages/main.tsx
+++ frontend/src/pages/main.tsx
@@ -1,7 +1,84 @@
-export default function Main (){
-    return(
-        <div>
-            <h1 className="title">Hola desde el archivo main</h1>
-        </div>
+import Image from "next/image"
+import logo1 from "./img/logo1.jpg";
+import logo2 from "./img/logo2.jpg";
+import granja from "./img/granja.jpeg";
+import f1 from "./img/f1.jpg";
+import f2 from "./img/f2.jpg";
+import f3 from "./img/f3.jpg";
+
+
+
+export default function Main() {
+    return (
+        <body className="fondo1">
+
+
+            <div className="diveslogan">
+                <br /><br /><br /><br /><br />
+                <center><p className="letra1">"OPTIMIZA TU GRANJA,</p></center><center><p className="letra1">MAXIMIZA TUS RESULTADOS"</p></center>
+            </div>
+
+            <div className="divimagen1">
+                <Image
+                src={logo1}
+                className="logo"
+                alt="logo"
+                ></Image>
+            </div>
+
+            <div className="divimagen2">
+            <Image
+                src={logo2}
+                className="logo"
+                alt="logo"
+                ></Image>
+            </div>
+
+            <div className="gestion">
+                <center><h2 className="letgestion">Gestion para tu agronegocio</h2></center>
+            </div>
+
+            <div className="divdesc">
+                <h2 className="letdes">EZ farm es tu página ideal para la administración de
+                    tu agronegocio, gestión de recursos para tus proyectos
+                    y agilización en informes de tus moviminetos. Una suscripción asequible te permitirá
+                    manejar tu empresa de una forma increible, garantizando una experiencia sencilla
+                    y eficiente, maximizando tus resultados.
+                </h2>
+            </div>
+
+            <Image
+                src={granja}
+                className="imggran"
+                alt="granja"
+                ></Image>
+
+            <Image
+                src={f1}
+                className="f1"
+                alt="f1"
+                ></Image>
+
+            <Image
+                src={f2}
+                className="f2"
+                alt="f2"
+                ></Image>
+
+            <Image
+                src={f3}
+                className="f3"
+                alt="f3"
+                ></Image>
+
+            <div className="d4">Maneja tus recursos</div>
+            <div className="d5">Mantente actualizado de tu negocio</div>
+            <div className="d6">Cumple tus resultados esperados</div>
+
+            <div className="div1"><h3>Administra y lleva un registro sobre todos los recursos disponibles en tu empresa, sean humanos o materiales.</h3></div>
+            <div className="div2"><h3>Entérate de las actualizaciones de cada departamento de tu negocio, recibiedno anuncios constantes e importantes.</h3></div>
+            <div className="div3"><h3>Con todas las funciones o herramientas te pondremos a disposicion, direccionarás tu negocio al éxito.</h3></div>
+
+        </body>
     )
 }
